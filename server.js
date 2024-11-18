7a50a41eb1b87a6de561f6fde6a3218f76274fab
Eloi <eloi.delva@gmail.com>
2024-11-17 22:13:07 -0500

    commit

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5500;
 
-// Serve static files (HTML, CSS, JS) from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 // Define a route to serve the index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
